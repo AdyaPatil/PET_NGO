7af4771808e41fc4d3e9140eb776e7bd8f50746f
AdyaPatil <161324212+AdyaPatil@users.noreply.github.com>
2025-04-07 14:30:00 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+export default {
+  server: {
+    allowedHosts: [
+      'a6dd6ee8e67d24525b12be22b7025e04-783236026.ap-south-1.elb.amazonaws.com'
+    ]
+  }
+}
+
