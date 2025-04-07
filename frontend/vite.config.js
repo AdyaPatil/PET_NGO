6910424d205a8a4873ee9699cb850ce60c3c1775
root <root@ip-10-1-1-12.ap-south-1.compute.internal>
2025-04-07 09:19:39 +0000

    update all code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default {
   server: {
-    allowedHosts: [
-      'a6dd6ee8e67d24525b12be22b7025e04-783236026.ap-south-1.elb.amazonaws.com'
-    ]
+    allowedHosts: 'all'
   }
 }
 
