5c62712caecc8093d120261966a31a377e1c6597
AdyaPatil <161324212+AdyaPatil@users.noreply.github.com>
2025-05-02 18:52:40 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
     host: '0.0.0.0',
     allowedHosts: [
       'abba39edb982a4cfebf55f1163ae2083-575612856.ap-south-1.elb.amazonaws.com'
-    ]
+    ],
+    hmr: {
+      overlay: false
+    }
   }
 })
